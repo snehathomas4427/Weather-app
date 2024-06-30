4b3af68c834fbb9537ee74ca34c42dd7180cab62
snehathomas4427 <snehavechoor@gmail.com>
2024-06-29 20:14:16 -0500

    starting

---
+++ main.py
@@ -0,0 +1,7 @@
+from website import create_app #website is a python package because of the __init__.py file inside it. 
+
+app = create_app()
+
+if __name__=='__main__':
+    app.run(debug='True') #this means this line will only be executed if you RUN main.py. debug=True means that everytime we make a change to the python file, it will rerun the web server.
+
---
+++ website/views.py
@@ -0,0 +1,19 @@
+#stores URL endpoints for the frontend part of the website
+#stores the routes (ex: login page, home page, etc.)
+
+from flask import Blueprint, render_template
+
+views = Blueprint('views' , __name__)
+
+#to define a route:
+@views.route('/') #for the home page. this function will run whenever we go to the '/' route 
+def home():
+    return render_template('home.html')
+
+@views.route('/today')
+def today():
+    return render_template('today.html')
+
+@views.route('/tenday')
+def tenday():
+    return render_template('tenday.html')
